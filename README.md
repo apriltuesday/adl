4832a29b6857561f899edf505278766d327e6570
April Shen <april.shen@import.io>
2016-03-22 20:37:38 +0000

    update

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ Watch this space.
 ![model]
 
 [model]: https://github.com/apriltuesday/adl/raw/master/model.png "model"
+
+### To-do:
+* decide char or word level
+* infrastructure for parameters
+* some parameters (sequence length, vocab size...) have to be set by data - how to handle?
+* how to deal with unknown max sequence length (i.e. no padding)
+* initializing weights
+* initial (unsupervised) training step
+* much later: add the query branch
+* first pass: if not labeled, query
--- network.py
+++ network.py
@@ -2,15 +2,12 @@ import numpy as np
 import pandas as pd
 
 from keras.models import Graph
-from keras.layers.recurrent import LSTM
-from keras.layers.core import AutoEncoder, Dense, Activation, TimeDistributedDense, Flatten, Dropout, RepeatVector
+from keras.layers.core import AutoEncoder, Dense, TimeDistributedDense, Flatten, Dropout
 from keras.layers.recurrent import LSTM
 from keras.layers.embeddings import Embedding
 
 from helper import generate_train_test_set
 from lstm_networks import generate_input_matrix, generate_target_vectors
-
-# maybe try using word2vec?
 
 
 class Network(object):
@@ -20,39 +17,46 @@ class Network(object):
     but should be generalizable.
     """
 
-    def __init__(self, params=None, initial_data=None):
+    def __init__(self, initial_data=None,
+                 embedding_size=10,
+                 encoding_size=30,
+                 decoding_size=10,
+                 dropout_rate=0.5):
         # Ideally we'd like to initialize things with some unlabelled data to train on
         # or alternatively initialize with word2vec pretrained embeddings
+
+        self.embedding_size = embedding_size
+        self.encoding_size = encoding_size
+        self.decoding_size = decoding_size
+        self.dropout_rate = dropout_rate
+        # data_dependent params
+        self.vocabulary_size = 196 # XXX if char level, can we set this before getting data?
+        self.sequence_length = 90 # XXX This isn't really doable online
+        
         self.model = self.get_model()
         self.unsup_mask = {'classification': 0}
-        # params:
-        #vocabulary_size = 196 # XXX this is input size, needs to get set somehow...... char level would make it easier
-        #embedding_size = 10
-        #encoding_size = 30
-        #decoding_size = 10
-        #sequence_length = 90 # XXX This isn't really doable online
 
 
     def get_model(self):
         model = Graph()
-        model.add_input(name='input', input_shape=(sequence_length,), dtype=int) # XXX or (vocabulary_size?)
+        model.add_input(name='input', input_shape=(self.sequence_length,), dtype=int)
 
         # Encoding input
-        model.add_node(Embedding(vocabulary_size, embedding_size, input_length=sequence_length),
+        model.add_node(Embedding(self.vocabulary_size, self.embedding_size, input_length=self.sequence_length),
                        name='embedding', input='input')
-        model.add_node(LSTM(encoding_size, input_length=sequence_length, return_sequences=True),
+        model.add_node(LSTM(self.encoding_size, input_length=self.sequence_length, return_sequences=True),
                        name='encoder', input='embedding')
 
         # Branch 1: reconstruct
-        model.add_node(LSTM(decoding_size, return_sequences=True),
+        model.add_node(LSTM(self.decoding_size, return_sequences=True),
                        name='decoder', input='encoder')
-        model.add_node(TimeDistributedDense(vocabulary_size, activation='softmax'),
+        model.add_node(TimeDistributedDense(self.vocabulary_size, activation='softmax'),
                        name='distributed', input='decoder')
         model.add_output(name='reconstruction', input='distributed')
 
         # Branch 2: classify
         model.add_node(Flatten(), name='flatten', input='encoder')
-        model.add_node(Dropout(0.5), name='dropout', input='flatten')
+        model.add_node(Dropout(self.dropout_rate), name='dropout', input='flatten')
         model.add_node(Dense(1, activation='sigmoid'),
                        name='dense', input='dropout')
         model.add_output(name='classification', input='dense')
@@ -61,21 +65,24 @@ class Network(object):
         # same as branch 2 but a different "label"
         # weights should be init so that we always query the 1st pt
 
-        model.compile(optimizer='rmsprop', loss={'reconstruction':'mse', 'classification':'binary_crossentropy'}) # XXX mse?
+        model.compile(optimizer='rmsprop', loss={'reconstruction':'mse',
+                                                 'classification': 'binary_crossentropy'})
         return model
 
 
+    # forward pass
     def predict(self, x):
         # NB online learning can't have sequence padding in any way that makes sense
         # assume x is a single point, for online/active purposes
         return self.model.predict({'input': x})
 
 
+    # backward pass
     def update_weights(self, x, y=None):
         # update weights for x
         # if no label y, no supervision
         # if label, the classification objective and the query objective both get updates
-        target = generate_target_vectors(x, vocabulary_size)
+        target = generate_target_vectors(x, self.vocabulary_size)
         if y:
             self.model.fit({'input': x,
                             'reconstruction': target,
